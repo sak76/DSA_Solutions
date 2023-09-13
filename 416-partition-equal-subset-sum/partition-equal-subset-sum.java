7705bed8b8e1e465b3dc937c056b48a69f36e132
sak76 <118853203+sak76@users.noreply.github.com>
2023-09-13 15:19:04 +0530

    Time: 35 ms (72.01%) | Memory: 46.7 MB (52.25%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -9,8 +9,34 @@ class Solution {
             return false;
         }
 
-        Boolean[][] memo = new Boolean[nums.length][sum/2 + 1];
-        return isSubsetSumEqualToTarget(nums, nums.length - 1, sum/2, memo);
+        boolean[][] dp = new boolean[nums.length][sum/2 + 1];
+        int n = nums.length;
+        int k = sum/2;
+
+        for(int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        for(int j = 0; j <= k; j++) {
+            if(nums[0] == j) {
+                dp[0][j] = true;
+            } 
+        }
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j <= k; j++) {
+                boolean take = false;
+                if(nums[i] <= j) {
+                    take =  dp[i - 1][j - nums[i]];
+                }
+
+                boolean notTake = dp[i - 1][j];
+
+                dp[i][j] = (take | notTake);
+            }
+        }
+
+        return dp[n-1][k]; 
     }
 
     private boolean isSubsetSumEqualToTarget(int[] nums, int index, int k, Boolean[][] memo) {
@@ -32,7 +58,6 @@ class Solution {
         }
 
         boolean notTake = isSubsetSumEqualToTarget(nums, index - 1, k, memo);
-
         return memo[index][k] = (take | notTake);
     }
 }
